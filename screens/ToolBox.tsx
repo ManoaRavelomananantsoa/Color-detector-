e444489aba63b89a77bfe26f711a05b5385738e5
Mathieu RAKOTOARISOA <148275593+Mathieu-Safidy@users.noreply.github.com>
2026-02-16 10:42:47 +0300

    toolbox file

---
+++ screens/ToolBox.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ToolBox = () => {
+  return (
+    <div>
+        <view>
+            <text>ToolBox</text>
+        </view>
+    </div>
+  )
+}
+
+export default ToolBox
